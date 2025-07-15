cdecc54bedec10ed9ee57e28ac01d681a7887298
HasarindaAmala <amalahasarinda1@gmail.com>
2025-07-16 00:35:37 +0530

    assignment done

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'fetures/SplashScreen/ui/SplashScreen.dart';
 
-void main(){
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+
+  ]);
   runApp(FoodOrderingApp());
 }
 
